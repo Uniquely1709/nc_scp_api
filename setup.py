b16a257a189c9c9bdea74ba05061ba9cf90cede1
Janneck <37475969+jdenda@users.noreply.github.com>
2021-02-17 14:22:40 +0100

    correct license

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/jdenda/nc_scp_api",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
